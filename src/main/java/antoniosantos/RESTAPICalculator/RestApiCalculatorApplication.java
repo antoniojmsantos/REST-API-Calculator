52a42198d0f6d42c469a2dc889c0cf83f4343c76
António Santos <santostoni10@hotmail.com>
2021-07-15 19:57:28 +0100

    Initial skeleton

---
+++ src/main/java/antoniosantos/RESTAPICalculator/RestApiCalculatorApplication.java
@@ -0,0 +1,13 @@
+package antoniosantos.RESTAPICalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/antoniosantos/RESTAPICalculator/RestApiCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package antoniosantos.RESTAPICalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
